e700b7f8d75823085a4b47db033f4de7040dae94
doncasanova <ethanjuan@yahoo.com>
2018-05-07 20:59:21 -0500

    changed the profile to a div entering from the left removed contact page removed profile page removed changed @media

---
+++ assets/JS/app.js
@@ -0,0 +1,14 @@
+$("body .showPortfol").on("click", function () {
+  console.log("slide out")
+
+  $('.slideinAbout').addClass('active');
+  //brings list div in
+  $('.slideOutAbout').addClass('active');
+})
+$("body .closeSlider").on("click", function () {
+  console.log("slide in")
+
+  $('.slideinAbout').removeClass('active');
+  //brings list div in
+  $('.slideOutAbout').removeClass('active');
+})
